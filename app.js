3b5701f0a1467ea7942c4ba740814291e20ce56a
Fais Rafii <fais.rafii111@gmail.com>
2021-10-05 15:09:42 +0700

    update

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
 var vertexShaderText = 
 [
-    'precision mediump float;',
-    '',
-    'attribute ver2 vertPosition;',
-    '',
-    'void main()',
-    '{',
-    'gl_position = vec4(vertPosition, 0.0, 1.0);',
-    '}'
+'precision mediump float;',
+'',
+'attribute vec2 vertPosition;',
+'attribute vec3 vertColor;',
+'varying vec3 fragColor;',
+'',
+'void main()',
+'{',
+'  fragColor = vertColor;',
+'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}'
 ].join('\n');
 
-var fragmentShaderText = 
+var fragmentShaderText =
 [
-    'precision mediump float;',
-    '',
-    'void main()',
-    '{',
-    'gl_fragColor = vec4(1.0, 0.0, 0.0, 1.0);',
-    '}'
+'precision mediump float;',
+'',
+'varying vec3 fragColor;',
+'void main()',
+'{',
+'  gl_FragColor = vec4(fragColor, 1.0);',
+'}'
 ].join('\n');
 
-var InitDemo = function(){
-    console.log("This is working")
-    /*1. Initialize WebGL */
-    var canvas = document.getElementById('game-surface');
-    var gl = canvas.getContext('webgl');
+var InitDemo = function () {
+	console.log('This is working');
 
-    if(!gl){ /*if the browser doesn't fully support*/
-        console.log('Webgl not supported, falling back on experimental-webgl');
-        gl = canvas.getContext('experimental-webgl');
-    }
+	var canvas = document.getElementById('game-surface');
+	var gl = canvas.getContext('webgl');
 
-    if(!gl){
-        alert('Your Browser Does Not Support WebGL')
-    
-    }
-    gl.clearColor(0.75, 0.85, 0.8, 1.0); /*Background Color sesuai width ama height (harusnya muncul ijo*/ 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); /*trigger buat munculin warna*/
-    };
-    
-//1.Declare Vertex Shader//
-var vertexShader = gl.createShader();
+	if (!gl) {
+		console.log('WebGL not supported, falling back on experimental-webgl');
+		gl = canvas.getContext('experimental-webgl');
+	}
 
-//2.Declare Fragment Shader//
-//3.Declare Source Of Both Shader//
-//4.Compile Both Shaders//
-//5.Attach To One Program//
+	if (!gl) {
+		alert('Your browser does not support WebGL');
+	}
 
+	gl.clearColor(0.75, 0.85, 0.8, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+	//
+	// Create shaders
+	// 
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
+	gl.shaderSource(vertexShader, vertexShaderText);
+	gl.shaderSource(fragmentShader, fragmentShaderText);
 
-}
+	gl.compileShader(vertexShader);
+	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+		return;
+	}
+
+	gl.compileShader(fragmentShader);
+	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+		return;
+	}
