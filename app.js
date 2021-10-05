92f101df753348a3bd16c09b02cb7b9f92627a8e
Fais Rafii <fais.rafii111@gmail.com>
2021-10-05 11:05:04 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+var vertexShaderText = 
+[
+    'precision mediump float;',
+    '',
+    'attribute ver2 vertPosition;',
+    '',
+    'void main()',
+    '{',
+    'gl_position = vec4(vertPosition, 0.0, 1.0);',
+    '}'
+].join('\n');
+
+var fragmentShaderText = 
+[
+    'precision mediump float;',
+    '',
+    'void main()',
+    '{',
+    'gl_fragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+    '}'
+].join('\n');
+
 var InitDemo = function(){
     console.log("This is working")
     /*1. Initialize WebGL */
@@ -11,4 +33,21 @@ var InitDemo = function(){
 
     if(!gl){
         alert('Your Browser Does Not Support WebGL')
+    
     }
+    gl.clearColor(0.75, 0.85, 0.8, /*Background Color sesuai width ama height (harusnya muncul ijo*/ 
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); /*trigger buat munculin warna*/
+    };
+    
+//1.Declare Vertex Shader//
+var vertexShader = gl.createShader();
+
+//2.Declare Fragment Shader//
+//3.Declare Source Of Both Shader//
+//4.Compile Both Shaders//
+//5.Attach To One Program//
+
+
+
+
+}
